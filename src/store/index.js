14d4707b63e276b04f873c9db10f3eb2a8ad9cc5
Miggy <mendozajuanmiguel11@gmail.com>
2019-07-31 18:52:12 +0800

    updated app look

--- src/store/index.js
+++ src/store/index.js
@@ -9,12 +9,18 @@ export default new Vuex.Store({
     fieldCount: 0
   },
   getters: {
-    GET_SONGS_ON_LIST: state => state.songs,
+    GET_SONG_LIST: state => state.songs,
     GET_FIELD_NUMS: state => state.fieldCount
   },
   mutations: {
     PUSH_SONG_TO_LIST(state, payload) {
       state.songs.push(payload);
+    },
+    REMOVE_SONG_FROM_LIST(state, payload) {
+      const songIndex = state.songs.findIndex(song => song === payload);
+      console.log(songIndex);
+      state.songs.splice(songIndex, 1);
+      console.log(state.songs);
     },
     INCREMENT_FIELD_COUNT(state) {
       state.fieldCount += 1;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,21 +14,38 @@
         </div>
       </v-layout>
 
-      <v-layout row v-for="n in numOfSongs" :key="n">
-        <SongTextbox :songNumber="n"/>
+      <v-layout row v-if="isSongListEmpty" justify-center align-center mt-5 mb-5>
+        <div class="primary--text headline">NO SONGS YET!!</div>
+      </v-layout>
+
+      <v-layout row align-center justify-center v-else v-for="(song, index) in songList" :key="song">
+        <v-flex xs6>
+          <songList :songTitle="song" :songNumber="index"/>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row align-baseline justify-center mt-4>
+        <v-flex xs4>
+          <v-text-field
+            v-model="song"
+            placeholder="Enter the title or artist of the desired song."
+            outlined
+            rounded
+            color="primary"
+          ></v-text-field>
+        </v-flex>
+        <v-flex xs1 align-content-center>
+            <v-btn color="primary" :disabled="song === ''" @click="addSong(song)">
+                <v-icon medium>done_outline</v-icon>
+            </v-btn>
+        </v-flex>
       </v-layout>
 
       <v-layout row align-baseline justify-center mt-5>
-        <v-flex xs3>
-          <v-btn color="secondary">
+        <v-flex xs6>
+          <v-btn color="secondary" :disabled="songList.length <= 0">
             <div>GENERATE LYRICS LIST</div>
             <v-icon right>save_alt</v-icon>
-          </v-btn>
-        </v-flex>
-        <v-flex xs3>
-          <v-btn color="primary" @click="addFields">
-            <div>ADD A SONG</div>
-            <v-icon right>add_circle_outline</v-icon>
           </v-btn>
         </v-flex>
       </v-layout>
@@ -39,26 +56,44 @@
 <script>
 // @ is an alias to /src
 import SongTextbox from "@/components/SongTextbox.vue"
+import songList from "@/components/SongList.vue"
 
 
 export default {
   name: "home",
   components: {
     SongTextbox,
+    songList
+  },
+  data() {
+    return {
+      song: ""
+    }
   },
   computed: {
     numOfSongs() {
-      let numberOfSongs = this.$store.getters.GET_SONGS_ON_LIST;
+      let numberOfSongs = this.$store.getters.GET_SONG_LIST;
       return numberOfSongs.length + 1;
-    }
+    },
+    songList() {
+      return this.$store.getters.GET_SONG_LIST;
+    },
+    isSongListEmpty() {
+      if(this.songList.length <= 0) return true;
+      else return false;
+    },
   },
   methods: {
     addSong(song) {
       this.$store.commit('PUSH_SONG_TO_LIST', song);
+      this.song = "";
     },
     addFields() {
       this.$store.commit('INCREMENT_FIELD_COUNT');
-    }
+    },
+    generateLyrics() {
+      console.log(this.songList);
+    },
   },
 };
 </script>
