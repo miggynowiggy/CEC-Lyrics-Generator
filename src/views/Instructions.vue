a91c8e8df9201ea9778e10aa99ce4c7e7116dec2
Miggy <mendozajuanmiguel11@gmail.com>
2019-07-31 19:06:00 +0800

    added an "instructions" page

--- src/views/Instructions.vue
+++ src/views/Instructions.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="instructions">
-    <h1>This is an about page</h1>
+    <v-layout row align-center justify-center>
+      <div class="headline primary--text font-weight-black">HOW TO USE?</div>
+    </v-layout>
+
+    <v-layout row wrap align-center justify-center mt-3>
+      <v-flex xs6>
+        <div class="body-1">
+          On the textbox, enter either the Title, or the Artist/Composer 
+          of the song you want to get the lyrics. As you type, song suggestions will be shown. 
+          Just click on any of the choices available on the list, and a pop-up dialog will show 
+          the lyrics of the selected song. To finally add the song to the list, just click the
+          <span class="font-weight-bold">Add Song</span> button. 
+          Once you have added all the necessary songs,
+          click the <span class="font-weight-bold">"Generate Lyrics List"</span> Button. 
+          Wait for the system to generate the file, and once done. The file will be automatically 
+          downloaded to your computer.
+        </div>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap align-center justify-center mt-5>
+      <div class="primary--text headline font-weight-black">CREATED BY:</div>
+    </v-layout>
+
+    <v-layout row wrap align-center justify-center>
+      <div>JUAN MIGUEL MENDOZA - Saint Augustine CEC</div>
+    </v-layout>
   </div>
 </template>
