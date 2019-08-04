0e25162130d95458405bf8ade95f2ca574e7aea2
Miggy <mendozajuanmiguel11@gmail.com>
2019-08-04 19:02:47 +0800

    improved layout and different viewport displays

--- src/components/SongList.vue
+++ src/components/SongList.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="songList">
-        <v-layout row align-baseline justify-center wrap mt-3 mb-3 pa-3 class="secondary lighten-4">
-            <v-flex xs2>
-                <div class="secondary--text subheading">Song #{{ songNumber + 1 }}</div>
+        <v-layout row wrap align-center justify-center mt-3 mb-3 pa-3 class="primary">
+            <v-flex xs12 md3 justify-content-start>
+                <div class="white--text subheading font-italic">Song #{{ songNumber + 1 }}</div>
             </v-flex>
-            <v-flex xs6>
-                <div class="primary--text">{{ songTitle }}</div>
+            <v-flex xs12 md5
+                :class="{'mt-2': $vuetify.breakpoint.smAndDown, 'mt-0': $vuetify.breakpoint.mdAndUp}"
+            >
+                <div class="white--text subheading font-weight-bold">{{ songTitle }}</div>
             </v-flex>
-            <v-flex xs1>
-                <v-btn outlined color="primary" @click="removeSong(songTitle)">
+            <v-flex xs12 md3 offset-md1
+                :class="{'mt-2': $vuetify.breakpoint.smAndDown, 'mt-0': $vuetify.breakpoint.mdAndUp}"
+            >
+                <v-btn outlined color="white" @click="removeSong(songTitle)">
                     <v-icon medium>close</v-icon>
+                    Remove
                 </v-btn>
             </v-flex>
         </v-layout>
